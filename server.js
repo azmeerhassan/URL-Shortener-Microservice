4c79fb07211a2005e21358637d8e7a3e95981db2
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-18 14:58:23 +0500

    Create /api/shorturl (POST) endpoint

--- server.js
+++ server.js
@@ -10,6 +10,27 @@ app.get('/', (req, res) => {
   res.send('URL Shortener Microservice is running 🚀');
 });
 
+const urls = []
+
+app.post('/api/shorturl', (req, res)=>{
+  const originalUrl = req.body.url
+  console.log('Received URL: ',originalUrl);
+  
+  const urlPattern = /^https?:\/\/.+/i;
+
+  if(!urlPattern.test(originalUrl)){
+    return res.json({error: "Invalid URL"})
+  }
+
+  const shortUrl = urls.length + 1
+  urls.push({original_url: originalUrl, short_url: shortUrl})
+
+  return res.json({
+    original_url: originalUrl, short_url: shortUrl
+  })
+})
+
+
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
